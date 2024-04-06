01219dc696dd880d1221bf8b9093d1fab3657cd6
Tamilore Adegbaye <doa325@lehigh.edu>
2024-04-06 09:08:53 -0400

    Added events

--- clubconnect/src/pages/Home.js
+++ clubconnect/src/pages/Home.js
@@ -22,6 +22,7 @@ export default function Home() {
       <NavBar />
       <SearchBar />
       <div className="event-container">
+      <FilteredEvents/>
         <LehighData />
         <LafayetteData />
         <MoravianData />
