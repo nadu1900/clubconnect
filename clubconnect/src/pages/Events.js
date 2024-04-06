31270aa98bb73e18ff21359d1e628ff5473e978a
Tamilore Adegbaye <doa325@lehigh.edu>
2024-04-06 03:54:11 -0400

    Added mock data

--- clubconnect/src/pages/Events.js
+++ clubconnect/src/pages/Events.js
@@ -1,15 +1,15 @@
-import NavBar from "../components/NavBar";
-import SearchBar from "../components/SearchBar";
-import React, { useState, useEffect } from 'react';
-import FilteredEvents from "../data/FilteredEvents";
+// import NavBar from "../components/NavBar";
+// import SearchBar from "../components/SearchBar";
+// import React, { useState, useEffect } from 'react';
+// import FilteredEvents from "../data/FilteredEvents";
 
-export default function Events(){
-    return(
-        <>
-        <h1>ClubConnect</h1>
-        <NavBar />
-        <SearchBar />
-        <FilteredEvents />
-        </>
-    )
-}
+// export default function Events(){
+//     return(
+//         <>
+//         <h1>ClubConnect</h1>
+//         <NavBar />
+//         <SearchBar />
+//         <FilteredEvents />
+//         </>
+//     )
+// }
