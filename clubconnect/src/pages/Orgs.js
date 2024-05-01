5b5cc4d5d788b004312fabb4c0d1ba3070a36f61
Nana-Abenaa Nyanor <nadu1900@gmail.com>
2024-05-01 14:51:01 -0400

    pushing changes

--- clubconnect/src/pages/Orgs.js
+++ clubconnect/src/pages/Orgs.js
@@ -4,13 +4,15 @@ import React, { useState, useEffect } from 'react';
 import FilteredEvents from "../data/FilteredEvents";
 import AllEvents from "../data/AllEvents";
 import EventManager from "../components/EventManager";
+import OrgManager from "../components/OrgManager";
+
 
 export default function Orgs(){
     return(
         <>
         <h1>ClubConnect</h1>
         <NavBar />
-        <EventManager/>
+        <OrgManager/>
    
         </>
     )
